73686f646fd2a81e986f0468f133503b6185b4e0
jrvladimir <vjcalanasan@gmail.com>
2022-02-23 10:41:47 +0800

    Inventory stored to a structure

--- View Inventory.cpp
+++ View Inventory.cpp
@@ -4,11 +4,17 @@
 #include <string.h>
 #define DELIMITER "\";,\n"
 
-
+struct element{
+    int id;
+    char description[100];
+    int quantity;
+    char date[100];
+    float price;
+};
 
 int main()
 {
-
+	struct element product[1000];
 	FILE* fp = fopen("Inventory_ST_NoBOM.csv", "r");
 	 if (!fp) {
         // Error in file opening
@@ -30,35 +36,38 @@ int main()
             while (value) {
                 // Column 1
                 if (column == 0) {
-                    printf("Inventory ID : ");
+                    product[row].id = strtol(value, NULL, 10);
                 }
   
                 // Column 2
                 if (column == 1) {
-                    printf("\tDescription : ");
+                    strcpy(product[row].description,value);
                 }
   
                 // Column 3
                 if (column == 2) {
-                    printf("\tQuantity :");
+                    product[row].quantity = strtol(value, NULL, 10);
                 }
                 
 				// Column 4
                 if (column == 3) {
-                    printf("\tDate :");
+                    strcpy(product[row].date,value);
                 }
                 
 				// Column 5
                 if (column == 4) {
-                    printf("\tPrice :");
+                    product[row].price = strtof(value, NULL);
                 }
-  
-                printf("%s", value);
                 value = strtok(NULL, DELIMITER);
                 column++;
             }
-  
+  			printf("id: %d ", product[row].id);
+  			printf("description: %s ", product[row].description);
+  			printf("quantity: %d ", product[row].quantity);
+  			printf("date: %s ", product[row].date);
+  			printf("price: %f ", product[row].price);
             printf("\n");
+            row++;
         }
         fclose(fp);
 
