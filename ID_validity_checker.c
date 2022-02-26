0209d079a7d33a6a1722a21b6ed9647f4d8e3326
jrvladimir <vjcalanasan@gmail.com>
2022-02-26 16:04:15 +0800

    Check ID if it is a 5 digit integer
    Doesn't check if the ID is already taken.

---
+++ ID_validity_checker.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "string.h"
+
+int main(){
+	char userInput[64];
+	int num = 0;
+	
+	while (num == 0) {
+		printf("Inventory ID (Enter a five digit interger):");
+		fgets(userInput, 63, stdin);
+		
+		if ( sscanf(userInput, "%d", &num) != 1 ){
+			num = 0;
+			//does not accept other data types
+			printf("Invalid input.\n");
+			continue;
+		}
+		
+		if( strlen(userInput) != 6 ){
+			//only accepts 5 digit integers and then null 
+			printf("Invalid input.\n");
+			continue;
+		}
+		
+		if ( num < 10000 || num > 99999 ){
+			num = 0;
+			printf("Invalid input.\n");
+			continue;
+		}else{
+			printf("Valid Inventory ID:%d", num);
+		}
+	}
+	
+	return 0;	
+}
