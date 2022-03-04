11cf01886ebf1ed0bc51b409a16da3d6d859041e
jrvladimir <vjcalanasan@gmail.com>
2022-03-04 14:37:55 +0800

    Fixed 00000
    00000 is invalid but should be valid fixed

--- ID_validity_checker.c
+++ ID_validity_checker.c
@@ -8,18 +8,22 @@ int* ID_validity_checker(char userInput[64]){
 	int num = -1;
 	int ID_output = 0;
 
-	int bilang;
-	char number[64];
-	sscanf(userInput, "%d", &bilang);
-	sprintf(number, "%d", bilang);
-	int bilang_l;
-	bilang_l = strlen(number) + 1;
+	int y, flag = 0;
+
+
+	while (userInput[y++] != '\0'){
+		if ( (userInput[y] >= '!' && userInput[y] == '/') || (userInput[y] >= ':' && userInput[y] <= '~' )){
+			flag = 1;
+			break;
+		}
+	}
+
 
 				
 		if( strlen(userInput) != 6 ){
 			//only accepts 5 digit integers and then null 
 			ID_output = 0;
-	    }else if ( strlen(userInput) != bilang_l){
+	    }else if ( flag == 1){
 	    	ID_output = 0;
 		}else if ( sscanf(userInput, "%d", &num) != 1 ){
 			//does not accept other data types
--- Quantity_validity_checker.c
+++ Quantity_validity_checker.c
@@ -10,25 +10,20 @@ int* Quantity_validity_checker(char userInput[64]){
 	int quantity_output = 0;
 	
 	//checks if the input is float
-	while (userInput[i++] != '\0'){
-		if (userInput[i] == '.'){
+	int y;
+
+
+	while (userInput[y++] != '\0'){
+		if ( (userInput[y] >= '!' && userInput[y] == '/') || (userInput[y] >= ':' && userInput[y] <= '~' )){
 			flag = 1;
 			break;
 		}
 	}
 	
-	int bilang;
-	char number[64];
-	sscanf(userInput, "%d", &bilang);
-	sprintf(number, "%d", bilang);
-	int bilang_l;
-	bilang_l = strlen(number) + 1;
-
-	
 	if( strlen(userInput) < 2 || strlen(userInput) > 8 ){
 		//doesn't accept empty input or greater than 7 digits
 		quantity_output = 0;
-	}else if ( strlen(userInput) != bilang_l){
+	}else if ( flag == 1){
 	    quantity_output = 0;	
 	}else if ( sscanf(userInput, "%d", &num ) != 1 ){
 		//does not accept other data types
